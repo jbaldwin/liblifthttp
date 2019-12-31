e70decf84a7ea6378d54b848a907334502b0553d
jbaldwin <joshrbaldwin@gmail.com>
2019-12-30 18:33:02 -0700

    Move query builder example to tests

---
+++ test/QueryBuilderTest.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "catch.hpp"
+
+#include <lift/QueryBuilder.hpp>
+
+TEST_CASE("Query Builder simple")
+{
+    lift::QueryBuilder query_builder{};
+    query_builder
+        .SetScheme("https")
+        .SetHostname("www.example.com")
+        .SetPort(443)
+        .AppendPathPart("test")
+        .AppendPathPart("path")
+        .AppendQueryParameter("param1", "value1")
+        .AppendQueryParameter("param2", "value2");
+
+    auto url = query_builder.Build();
+
+    REQUIRE(url == "https://www.example.com:443/test/path?param1=value1&param2=value2");
+
+    query_builder
+        .SetScheme("http")
+        .SetHostname("www.reddit.com")
+        .AppendPathPart("r")
+        .AppendPathPart("funny");
+    url = query_builder.Build();
+
+    REQUIRE(url == "http://www.reddit.com/r/funny");
+}
--- test/main.cpp
+++ test/main.cpp
@@ -6,6 +6,7 @@
 static lift::GlobalScopeInitializer g_lift_gsi{};
 
 #include "EscapeTest.hpp"
+#include "QueryBuilderTest.hpp"
 #include "SyncRequestTest.hpp"
 #include "AsyncRequestTest.hpp"
 #include "TransferProgressRequest.hpp"
